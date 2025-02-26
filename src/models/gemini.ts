41f52423fcc6a5183d8018de69341296d93132f8
islemci <musti@duck.com>
2025-02-26 19:31:55 +0300

    fix gemini api

--- src/models/gemini.ts
+++ src/models/gemini.ts
@@ -12,7 +12,7 @@ const cache = new Map<string, { response: string; timestamp: number }>();
 const CACHE_TTL = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
 
 const model = genAI.getGenerativeModel({
-  model: 'gemini-2.0-flash-lite-preview-02-05',
+  model: 'gemini-2.0-flash-lite-001',
   systemInstruction:
     "You are Karakulak, a helpful AI agent working with Tekir search engine. You will receive some questions and try to answer them in a short paragraph. Make sure that you state facts. If you can't or don't want to answer a question, if you think it is against your Terms of Service, if you think that the searched term is not a question or if you can't find information on the question or you don't understand it, say: \"Sorry, I can't help you with that.\" or its equivalent in the input's language."
 });
